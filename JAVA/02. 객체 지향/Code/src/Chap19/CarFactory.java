e1084e94c4da24e942faa387b6de945760488c11
kho903 <gmldnr2222@naver.com>
2021-07-26 21:54:02 +0900

    static과 싱글톤 패턴 예제

---
+++ JAVA/02. 객체 지향/Code/src/Chap19/CarFactory.java
@@ -0,0 +1,16 @@
+package Chap19;
+
+public class CarFactory {
+    private static CarFactory instance = new CarFactory();
+
+    public static CarFactory getInstance(){
+        if (instance == null)
+            instance = new CarFactory();
+        return instance;
+    }
+
+    public Car createCar() {
+
+        return new Car();
+    }
+}
