2bf3d6aea7676eba2a60691bcbb74c3edfec1ad0
kho903 <gmldnr2222@naver.com>
2021-07-28 14:09:37 +0900

    21. 객체 배열 사용하기

---
+++ JAVA/02. 객체 지향/Code/src/Chap21/ObjectCopyTest.java
@@ -0,0 +1,44 @@
+package Chap21;
+
+public class ObjectCopyTest {
+    // 얕은 복사
+    public static void main(String[] args) {
+        Book[] library = new Book[5];
+        Book[] copyLibrary = new Book[5];
+
+        library[0] = new Book("태백산맥1", "조정래");
+        library[1] = new Book("태백산맥2", "조정래");
+        library[2] = new Book("태백산맥3", "조정래");
+        library[3] = new Book("태백산맥4", "조정래");
+        library[4] = new Book("태백산맥5", "조정래");
+
+        System.arraycopy(library, 0, copyLibrary, 0, 5);
+
+//        System.out.println("=====library=====");
+//        for (Book book : library) {
+//            System.out.println(book);
+//            book.showInfo();
+//        }
+//
+//        System.out.println("=====Copy library=====");
+//        for (Book book : copyLibrary) {
+//            System.out.println(book);
+//            book.showInfo();
+//        }
+        library[0].setAuthor("박완서");
+        library[0].setTitle("나목");
+
+        System.out.println("=====library=====");
+        for (Book book : library) {
+            System.out.println(book);
+            book.showInfo();
+        }
+        // 같이 바뀜
+        System.out.println("=====Copy library=====");
+        for (Book book : copyLibrary) {
+            System.out.println(book);
+            book.showInfo();
+        }
+
+    }
+}
