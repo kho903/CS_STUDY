1036200e822ba5f72e509357b3e6d1ed6d51487b
kho903 <gmldnr2222@naver.com>
2021-08-05 19:36:25 +0900

    10. 추상 클래스를 활용한 템플릿 메서드 패턴

---
+++ JAVA/03. 객체 지향 핵심/Code/src/Chap10/CarTest.java
@@ -0,0 +1,12 @@
+package Chap10;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car aiCar = new AICar();
+        aiCar.run();
+
+        System.out.println("=================");
+        Car mCar = new ManualCar();
+        mCar.run();
+    }
+}
