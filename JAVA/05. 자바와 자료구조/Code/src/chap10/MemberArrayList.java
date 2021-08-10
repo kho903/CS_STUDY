cecf6728eadb0ece945421bfacc9f9db0097c007
kho903 <gmldnr2222@naver.com>
2021-08-10 17:32:12 +0900

    05. 자바와 자료구조 / 11. Collection 요소를 순회하는 Iterator

--- JAVA/05. 자바와 자료구조/Code/src/chap10/MemberArrayList.java
+++ JAVA/05. 자바와 자료구조/Code/src/chap10/MemberArrayList.java
@@ -1,6 +1,7 @@
 package chap10;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
     private ArrayList<Member> arrayList;
@@ -18,11 +19,20 @@ public class MemberArrayList {
     }
 
     public boolean removeMember(int memberId) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            Member member = arrayList.get(i);
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            Member member = arrayList.get(i);
+//            int tempId = member.getMemberId();
+//            if (tempId == memberId) {
+//                arrayList.remove(i);
+//                return true;
+//            }
+//        }
+        Iterator<Member> ir = arrayList.iterator();
+        while (ir.hasNext()) {
+            Member member = ir.next();
             int tempId = member.getMemberId();
             if (tempId == memberId) {
-                arrayList.remove(i);
+                arrayList.remove(member);
                 return true;
             }
         }
