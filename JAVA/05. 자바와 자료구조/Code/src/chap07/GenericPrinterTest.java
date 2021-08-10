64843bcae695159340012ea811d6f4c547e44e2c
kho903 <gmldnr2222@naver.com>
2021-08-10 15:30:50 +0900

    05. 자바와 자료구조 / 07. <T extends 클래스>  사용하기 Code

---
+++ JAVA/05. 자바와 자료구조/Code/src/chap07/GenericPrinterTest.java
@@ -0,0 +1,16 @@
+package chap07;
+
+public class GenericPrinterTest {
+    public static void main(String[] args) {
+        Powder powder = new Powder();
+        GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+        powderPrinter.setMaterial(powder);
+
+        Powder p = powderPrinter.getMaterial();
+        System.out.println(powderPrinter);
+
+        GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+        plasticPrinter.setMaterial(new Plastic());
+        System.out.println(plasticPrinter);
+    }
+}
---
+++ JAVA/05. 자바와 자료구조/Code/src/chap07/Material.java
@@ -0,0 +1,5 @@
+package chap07;
+
+public abstract class Material {
+    public abstract void doPrinting();
+}
