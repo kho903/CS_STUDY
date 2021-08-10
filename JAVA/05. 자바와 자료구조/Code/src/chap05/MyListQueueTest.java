011ee22692178f3feabac821cdb17a3939c02177
kho903 <gmldnr2222@naver.com>
2021-08-10 14:24:05 +0900

    05. 자바와 자료구조 / 05. Queue 구현하기

---
+++ JAVA/05. 자바와 자료구조/Code/src/chap05/MyListQueueTest.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class MyListQueueTest {
+    public static void main(String[] args) {
+        MyLinkedQueue listQueue = new MyLinkedQueue();
+        listQueue.enQueue("A");
+        listQueue.enQueue("B");
+        listQueue.enQueue("C");
+        listQueue.printAll();
+
+        System.out.println(listQueue.deQueue());
+        System.out.println(listQueue.deQueue());
+        listQueue.printAll();
+    }
+}
