dbfb70d0e226cacc76d1ad415ad4f5f1cb97e85b
kho903 <gmldnr2222@naver.com>
2021-08-11 16:24:50 +0900

    06. 자바의 다양한 기능들 / 01. 여러 내부클래스의 정의와 유형

---
+++ JAVA/06. 자바의 다양한 기능들/Code/src/chap01/InnerTest.java
@@ -0,0 +1,68 @@
+package chap01;
+
+class OutClass {
+    private int num = 10;
+    private static int sNum = 20;
+    private InClass inClass;
+
+    public OutClass() {
+        inClass = new InClass();
+    }
+
+    private class InClass {
+//    class InClass {
+        int iNum = 100;
+
+//        static int sInNum = 500; // 오류
+
+        void inTest() {
+            System.out.println("OutClass num = " + num + "(외부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass inNum = " + iNum + "(내부 클래스의 인스턴스 변수)");
+        }
+    }
+
+    public void usingClass() {
+        inClass.inTest();
+    }
+
+    static class InStaticClass {
+        int iNum = 100;
+        static int sInNum = 200;
+        void inTest() {
+            // 정적 클래스 내부의 일반 메서드에서는 외부 클래스 인스턴스 변수 사용 불가
+//            System.out.println("OutClass num = " + num + "(외부 클래스의 인스턴스 변수");
+            System.out.println("InClass num = " + iNum + "(내부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass sNum = " + sInNum + "(내부 클래스의 스태틱 변수)");
+        }
+
+        static void sTest() {
+            //(내부 클래스의 인스턴스 변수 샤용 불가
+//            System.out.println("InClass num = " + iNum + "(내부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass sNum = " + sInNum + "(내부 클래스의 스태틱 변수)");
+        }
+    }
+}
+
+public class InnerTest {
+    public static void main(String[] args) {
+        /*
+        OutClass outClass = new OutClass();
+        outClass.usingClass();
+
+        System.out.println();
+        // InClass에 private이 붙지 않으면 호출 가능
+//        OutClass.InClass inner = outClass.new InClass();
+//        inner.inTest();
+         */
+
+        OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+        sInClass.inTest();
+
+        System.out.println();
+
+        OutClass.InStaticClass.sTest();
+    }
+}
