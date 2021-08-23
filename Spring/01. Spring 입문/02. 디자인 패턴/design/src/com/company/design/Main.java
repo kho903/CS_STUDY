46cb477a421b679f75f5c7898ea6d1f45758c236
kho903 <gmldnr2222@naver.com>
2021-08-23 15:43:07 +0900

    스프링 입문 / 02. 디자인 패턴 / 05. 데코레이터(decorator) 패턴

--- Spring/01. Spring 입문/02. 디자인 패턴/design/src/com/company/design/Main.java
+++ Spring/01. Spring 입문/02. 디자인 패턴/design/src/com/company/design/Main.java
@@ -2,6 +2,11 @@ package com.company.design;
 
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.A3;
+import com.company.design.decorator.A4;
+import com.company.design.decorator.A5;
+import com.company.design.decorator.Audi;
+import com.company.design.decorator.ICar;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
@@ -54,7 +59,7 @@ public class Main {
         browser.show();
         browser.show();
         browser.show();
-         */
+
 
         AtomicLong start = new AtomicLong();
         AtomicLong end = new AtomicLong();
@@ -75,6 +80,23 @@ public class Main {
 
         aopBrowser.show();
         System.out.println("loading time : " + end.get());
+
+         */
+
+        // decorator
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        // a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+        // a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+
+        // a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();
     }
 
     public static void connect(Electronic110V electronic110V) {
