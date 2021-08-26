3a8438ac85c72181286139bc30d5f5add8b964b5
kho903 <gmldnr2222@naver.com>
2021-08-26 14:17:23 +0900

    Java / DataStructure / 단일 연결리스트2

--- JAVA/DataStructure/src/dataStructure/linkedList/NodeTest.java
+++ JAVA/DataStructure/src/dataStructure/linkedList/NodeTest.java
@@ -17,5 +17,48 @@ public class NodeTest {
         System.out.println(MyLinkedList.head.next.data);
         System.out.println();
         MyLinkedList.printAll();
+        System.out.println();
+
+        System.out.println("===링크드 리스트 생성하고, 1, 2, 3 데이터 넣기===");
+        SingleLinkedList<Integer> MyLinkedList2 = new SingleLinkedList<Integer>();
+        MyLinkedList2.addNode(1);
+        MyLinkedList2.addNode(2);
+        MyLinkedList2.addNode(3);
+        MyLinkedList2.printAll();
+
+        System.out.println("===1 데이터 뒤에 5 넣어 보기===");
+        MyLinkedList2.addNodeInside(5, 1);
+        MyLinkedList2.printAll();
+
+        System.out.println("===3 데이터 뒤에 6 넣어 보기===");
+        MyLinkedList2.addNodeInside(6, 3);
+        MyLinkedList2.printAll();
+
+        System.out.println("===없는 데이터를 찾도록 해서, 맨 마지막에 데이터 넣기===");
+        MyLinkedList2.addNodeInside(7, 20);
+        MyLinkedList2.printAll();
+
+
+        System.out.println("====테스트 : 5개의 노드 생성====");
+        SingleLinkedList<Integer> MyLinkedList3 = new SingleLinkedList<Integer>();
+        MyLinkedList3.addNode(1);
+        MyLinkedList3.addNode(2);
+        MyLinkedList3.addNode(3);
+        MyLinkedList3.addNode(4);
+        MyLinkedList3.addNode(5);
+        MyLinkedList3.printAll();
+
+        System.out.println(MyLinkedList3.delNode(3));
+        MyLinkedList3.printAll();
+
+        System.out.println(MyLinkedList3.delNode(1));
+        MyLinkedList3.printAll();
+
+        System.out.println(MyLinkedList3.delNode(5));
+        MyLinkedList3.printAll();
+
+        System.out.println(MyLinkedList3.delNode(20));
+        MyLinkedList3.printAll();
+
     }
 }
