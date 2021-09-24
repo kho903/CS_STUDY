e81175e538e6d31ffd8acff3951776aa545bb903
kho903 <gmldnr2222@naver.com>
2021-09-24 17:24:48 +0900

    Spring 입문 / 04. 스프링 부트 시작하기 / ObjectMapper 모범사례

--- Spring/01. Spring 입문/04. 스프링 부트 시작하기/07. 모범사례 - Object Mapper/object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
+++ Spring/01. Spring 입문/04. 스프링 부트 시작하기/07. 모범사례 - Object Mapper/object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.objectmapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ObjectMapperApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("============");
+
+        // Text JSON -> object
+        // object -> Text JSON
+
+        // controller req json(text) -> object
+        // response object -> json(text)
+
+        var objectMapper = new ObjectMapper();
+
+        // object -> text
+        // object mapper get method 를 활용한다.
+        var user = new User("kim", 20, "010-1111-2222");
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        // text -> object
+        // object mapper 는 default 생성자를 필요로 한다.
+        var objectUser = objectMapper.readValue(text, User.class);
+        System.out.println(objectUser);
+
     }
 
 }
