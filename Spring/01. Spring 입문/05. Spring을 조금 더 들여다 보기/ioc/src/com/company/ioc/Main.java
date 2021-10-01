4c3bb5e97cc83dc9bcb5193be6f49b3594d2cb56
kho903 <gmldnr2222@naver.com>
2021-10-01 20:56:35 +0900

    Spring 입문 / 05. Spring을 조금 더 들여다 보기 / 02. IoC, DI - DI

---
+++ Spring/01. Spring 입문/05. Spring을 조금 더 들여다 보기/ioc/src/com/company/ioc/Main.java
@@ -0,0 +1,24 @@
+package com.company.ioc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
+
+//        // Base 64 encoding
+//        IEncoder encoder = new Base64Encoder();
+//        String result = encoder.encode(url);
+//        System.out.println(result);
+
+//        IEncoder urlEncoder = new UrlEncoder();
+//        String urlResult = urlEncoder.encode(url);
+//        System.out.println(urlResult);
+
+        // DI
+        Encoder encoder = new Encoder(new Base64Encoder());
+//        Encoder encoder = new Encoder(new UrlEncoder());
+        String result = encoder.encode(url);
+        System.out.println(result);
+
+    }
+}
