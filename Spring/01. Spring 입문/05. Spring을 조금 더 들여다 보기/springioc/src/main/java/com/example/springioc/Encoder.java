6b401cf83e976eb247531f941531f822df931ae4
kho903 <gmldnr2222@naver.com>
2021-10-01 21:41:35 +0900

    Spring 입문 / 05. Spring을 조금 더 들여다 보기 / 02. IoC, DI - IoC

---
+++ Spring/01. Spring 입문/05. Spring을 조금 더 들여다 보기/springioc/src/main/java/com/example/springioc/Encoder.java
@@ -0,0 +1,21 @@
+package com.example.springioc;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Encoder {
+    private IEncoder iEncoder;
+
+    public Encoder(@Qualifier("base64Encoder") IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public void setIEncoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+}
