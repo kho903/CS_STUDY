6eecefdb4d78c4c40ee71c83ad0d5d427f5745c7
kho903 <gmldnr2222@naver.com>
2021-10-18 14:21:05 +0900

    Spring 입문 / 05. Spring을 조금 더 들여다 보기 / 04. AOP 실무사례 1

---
+++ Spring/01. Spring 입문/05. Spring을 조금 더 들여다 보기/aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -0,0 +1,26 @@
+package com.example.aop.controller;
+
+import com.example.aop.dto.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+    @GetMapping("/get/{id}")
+    public String get(@PathVariable Long id, @RequestParam String name) {
+        return id + " " + name;
+    }
+
+    @PostMapping("/post")
+    public User post(@RequestBody User user) {
+        System.out.println("post Method : " + user);
+        return user;
+    }
+}
