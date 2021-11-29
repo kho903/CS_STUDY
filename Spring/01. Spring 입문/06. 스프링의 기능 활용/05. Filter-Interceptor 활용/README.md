4c670f2607ec85e89927fe2c7dca511891aad5d2
kho903 <gmldnr2222@naver.com>
2021-11-29 16:50:07 +0900

    Spring 입문 / 06. 스프링의 기능 활용 / 05. Filter-Interceptor의 활용 - 2

--- Spring/01. Spring 입문/06. 스프링의 기능 활용/05. Filter-Interceptor 활용/README.md
+++ Spring/01. Spring 입문/06. 스프링의 기능 활용/05. Filter-Interceptor 활용/README.md
@@ -7,3 +7,8 @@ Spring에 의해서 데이턱 변환되기 전의 순수한 Client의 요청 / �
 - 주로 Spring Framework 에서는 request / response의 Logging 용도로 활용하거나,
 인증과 관련된 Logic들을 해당 Filter에서 처리한다.
 - 이를 선 / 후 처리 함으로써, Service business logic과 분리 시킨다.
+
+## Interceptor
+- Interceptor란 Filter와 매우 유사한 형태로 존재하지만, 차이점은 Spring Contect에 등록된다.
+- AOP와 유사한 기능을 제공할 수 있으며, 주로 인증 단게를 처리하거나, Logging을 하는 데에 사용한다.
+- 이를 선 / 후 처리함으로써, Service business logic과 분리 시킨다.
---
+++ Spring/01. Spring 입문/06. 스프링의 기능 활용/interceptor/src/main/java/com/example/interceptor/controller/PublicController.java
@@ -0,0 +1,19 @@
+package com.example.interceptor.controller;
+
+import com.example.interceptor.annotation.Auth;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+@Slf4j
+public class PublicController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("public hello Controller");
+        return "public hello";
+    }
+}
