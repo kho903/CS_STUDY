5d6afff6b84501723d0d80e1d06d89df0c416f7c
kho903 <gmldnr2222@naver.com>
2021-11-29 15:11:27 +0900

    Spring 입문 / 06. 스프링의 기능 활용 / 04. Spring Boot Validation 모범 사례

--- Spring/01. Spring 입문/06. 스프링의 기능 활용/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ Spring/01. Spring 입문/06. 스프링의 기능 활용/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
-
-        int a = 10 + age;
 
         return user;
     }
@@ -36,9 +45,9 @@ public class ApiController {
     }
 
     // Controller에 들어간 ExceptionHandler가 Global한 ExceptionHandler보다 우선순위
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println("api controller");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+//    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        System.out.println("api controller");
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
 }
