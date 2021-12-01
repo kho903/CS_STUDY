8193f6fb517663434400e337213906d9138d44c4
kho903 <gmldnr2222@naver.com>
2021-12-01 13:41:37 +0900

    Spring 입문 / 09. Swagger를 통한 API 공개 / 02. Swagger 설정하기 2

---
+++ Spring/01. Spring 입문/09. Swagger를 통한 API 공개/swagger/src/main/java/com/example/swagger/dto/UserRes.java
@@ -0,0 +1,13 @@
+package com.example.swagger.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserRes {
+    private String name;
+    private int age;
+}
