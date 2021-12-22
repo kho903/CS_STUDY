31e12c30a4bdc1e2f654f9449c87bf9ac0f9c6f5
kho903 <gmldnr2222@naver.com>
2021-12-22 13:15:58 +0900

    del bookmanager

--- Spring/JPA/bookmanager/src/main/java/com/jikim/jpa/bookmanager/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jikim.jpa.bookmanager.repository;
-
-import com.jikim.jpa.bookmanager.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
