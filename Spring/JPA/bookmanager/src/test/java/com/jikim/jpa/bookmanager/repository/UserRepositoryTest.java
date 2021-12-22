22eab5e180f587ead114e21fccbbe7c1ecf5803e
kho903 <gmldnr2222@naver.com>
2021-12-22 15:20:04 +0900

    JPA / 02. Spring Data JPA 기초 / 02. Repository Interface 메소드 실습 1

--- Spring/JPA/bookmanager/src/test/java/com/jikim/jpa/bookmanager/repository/UserRepositoryTest.java
+++ Spring/JPA/bookmanager/src/test/java/com/jikim/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.jikim.jpa.bookmanager.repository;
 
 import com.jikim.jpa.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -13,9 +15,32 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    void crud() {
-        userRepository.save(new User());
+    void findTest() {
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 3L, 5L));
 
-        System.out.println("userRepository.findAll() = " + userRepository.findAll());
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void saveTest() {
+        User user1 = new User("jack", "jack@naver.com");
+        User user2 = new User("steve", "steve@naver.com");
+
+        userRepository.save(user1);
+//        userRepository.saveAll(Lists.newArrayList(user1, user2));
+
+        List<User> users = userRepository.findAll();
+
+        users.forEach(System.out::println);
+    }
+
+    @Test
+//    @Transactional
+    void getTest() {
+//        User user = userRepository.getOne(1L);
+        User user = userRepository.findById(1L).orElse(null);
+
+        System.out.println("user = " + user);
     }
 }
