6c9d7199d4cbe0ff2cb9e2a5190b601467682ad8
kho903 <gmldnr2222@naver.com>
2021-12-22 21:57:24 +0900

    JPA / 04. Entity 기본 속성 / 01. Entity 기본 속성 (annotation) 1

--- Spring/JPA/bookmanager/src/main/java/com/jikim/jpa/bookmanager/domain/User.java
+++ Spring/JPA/bookmanager/src/main/java/com/jikim/jpa/bookmanager/domain/User.java
@@ -15,7 +15,10 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -25,6 +28,7 @@ import java.util.List;
 @Data
 @Builder
 @Entity
+@Table(name = "user", indexes = {@Index(columnList = "name")}, uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
 public class User {
     @Id
     @GeneratedValue
