6fc4f0b2a447af33a2a7624281a750d6831fae6f
kho903 <gmldnr2222@naver.com>
2022-01-29 20:34:44 +0900

    Functional Programming / 02. Functional Interface / 04. Predicate

---
+++ JAVA/functional_programming/src/com/company/functional/C02FunctionalInterface/C04Predicate.java
@@ -0,0 +1,35 @@
+package com.company.functional.C02FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class C04Predicate {
+
+    public static void main(String[] args) {
+        Predicate<Integer> isPositive = x -> x > 0;
+        System.out.println(isPositive.test(10));
+        System.out.println(isPositive.test(-10));
+
+        List<Integer> inputs = Arrays.asList(1, -5, 4, -9, 0, 2);
+        System.out.println("Positive number : "
+                + filter(inputs, isPositive));
+        System.out.println("Non-positive number : "
+                + filter(inputs, isPositive.negate()));
+        System.out.println("Non-negative number : "
+                + filter(inputs, isPositive.or(x -> x == 0)));
+        System.out.println("Positive even numbers : "
+                + filter(inputs, isPositive.and(x -> x % 2 == 0)));
+    }
+
+    public static <T> List<T> filter(List<T> inputs, Predicate<T> condition) {
+        List<T> output = new ArrayList<>();
+        for (T input : inputs) {
+            if (condition.test(input)) {
+                output.add(input);
+            }
+        }
+        return output;
+    }
+}
