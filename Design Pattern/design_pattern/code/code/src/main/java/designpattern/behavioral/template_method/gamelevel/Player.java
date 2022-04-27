44ae2e0b64f67571b14666518f9d2b5211b1aa29
kho903 <gmldnr2222@naver.com>
2022-04-27 21:45:40 +0900

    디자인패턴 / 02. 하위 클래스에 위임하기 / 01. Template Method - 구체적인 처리를 하위 클래스에 위임

---
+++ Design Pattern/design_pattern/code/code/src/main/java/designpattern/behavioral/template_method/gamelevel/Player.java
@@ -0,0 +1,25 @@
+package designpattern.behavioral.template_method.gamelevel;
+
+public class Player {
+
+	private PlayerLevel level;
+
+	public Player() {
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+	}
+
+	public void upgradeLevel(PlayerLevel level) {
+		level.showLevelMessage();
+		this.level = level;
+	}
+
+	public PlayerLevel getPlayerLevel() {
+		return level;
+	}
+
+	public void play(int count) {
+		level.go(count);
+	}
+
+}
