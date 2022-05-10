6230f4d49041a01ff8d986ae89f7dc619e68e558
kho903 <gmldnr2222@naver.com>
2022-05-10 14:43:31 +0900

    디자인패턴 / 06. 조건에 따른 여러가지 상태를 클래스로 표현하여 관리하는 패턴 / 02. Observer - 지켜보다 상태가 변화 하면 통지함

---
+++ Design Pattern/design_pattern/code/code/src/main/java/designpattern/behavioral/observer/GraphObserver.java
@@ -0,0 +1,20 @@
+package designpattern.behavioral.observer;
+
+public class GraphObserver implements Observer {
+	@Override
+	public void update(NumberGenerator generator) {
+		System.out.println("GraphObserver");
+		int number = generator.getNumber();
+
+		for (int i = 0; i < number; i++) {
+			System.out.print("*");
+		}
+		System.out.println();
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ Design Pattern/design_pattern/code/code/src/main/java/designpattern/behavioral/observer/Observer.java
@@ -0,0 +1,5 @@
+package designpattern.behavioral.observer;
+
+public interface Observer {
+	public void update(NumberGenerator generator);
+}
