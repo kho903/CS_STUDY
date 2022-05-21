ef8bf5a5b457f432ae16ee5e46acc1f6d5fd5d65
kho903 <gmldnr2222@naver.com>
2022-05-21 18:34:49 +0900

    디자인패턴 / 08. 구조 안을 돌아다니기 / 03. Chain of responsibility - 책임을 떠넘기기

---
+++ Design Pattern/design_pattern/code/code/src/main/java/designpattern/behavioral/responsibility/LimitSupport.java
@@ -0,0 +1,16 @@
+package designpattern.behavioral.responsibility;
+
+public class LimitSupport extends Support {
+
+	private int limit;
+
+	public LimitSupport(String name, int limit) {
+		super(name);
+		this.limit = limit;
+	}
+
+	@Override
+	protected boolean resolve(Trouble trouble) {
+		return trouble.getNumber() < limit;
+	}
+}
